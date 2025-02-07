bf0a33060130b038ae7eb3f0ff40b4ce9bd573cf
NextGoogleCEO-code <aneekeshyadav@gmail.com>
2025-02-08 02:00:50 +0530

    urls.py

--- postscribers/blog/urls.py
+++ postscribers/blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('blog/',views.index,name = 'blog-index'),
+    path('post_detail/<int:post_id>/',views.post_detail,name = 'blog-post_detail'),
+    path('post_edit/<int:post_id>/',views.post_edit,name = 'blog-post_edit'),
+    path('post_delete/<int:post_id>/',views.post_delete,name = 'blog-post_delete'),
     
 ]
 
